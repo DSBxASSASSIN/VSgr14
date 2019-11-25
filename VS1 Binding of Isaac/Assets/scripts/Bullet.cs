a25e88abdd193c479f1d9f8c7663a9c98be8a574
thijs van slooten <41576972+DSBxASSASSIN@users.noreply.github.com>
2019-11-25 18:25:41 +0100

    hope it does not break

--- VS1 Binding of Isaac/Assets/scripts/Bullet.cs
+++ VS1 Binding of Isaac/Assets/scripts/Bullet.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
-{
+public class Bullet : MonoBehaviour {
     public GameObject hitEffect;
+
+
+    void Start() {
+        Destroy(gameObject, 2f);
+    }
 
     void OnCollisionEnter2D(Collision2D collision) {
         GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 5f);
+        Destroy(effect, .2f);
         Destroy(gameObject);
     }
-}
+
+    void DestroyObjectDelayed() {
+        Destroy(gameObject, 5);
+    }
+
+}  
--- VS1 Binding of Isaac/Assets/scripts/Shooting.cs
+++ VS1 Binding of Isaac/Assets/scripts/Shooting.cs
@@ -12,11 +12,41 @@ public class Shooting : MonoBehaviour
 
     void Update(){
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            Shoot();
+            ShootUp();
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            ShootDown();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            ShootLeft();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            ShootRight();
         }
     }
 
-    void Shoot() {
+    void ShootUp() {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+
+    void ShootDown() {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.down * bulletForce, ForceMode2D.Impulse);
+    }
+
+    void ShootRight() {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+
+    void ShootLeft() {
         GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
         Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
         rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
